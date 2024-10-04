4f4fc0b4f50cd09e4acab8fa2bea43c031e6f0d3
yy-lab0795 <yanagisawa.y.ag@m.titech.ac.jp>
2024-10-04 21:59:41 +0900

    Commit

---
+++ web/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders title', () => {
+  render(<App />);
+  const titleElement = screen.getByText("Hello App");
+  expect(titleElement).toBeInTheDocument();
+});
